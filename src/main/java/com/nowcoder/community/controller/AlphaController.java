46fa298a84a5d299a68bca940a68ffe8073d66c0
简心静 <296828558@qq.com>
2021-11-20 12:29:23 +0800

    验证码功能完成

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -1,6 +1,7 @@
 package com.nowcoder.community.controller;
 
 import com.nowcoder.community.service.AlphaService;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -8,8 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
@@ -110,9 +113,9 @@ public class AlphaController {
     @ResponseBody
     public Map<String, Object> getEmp() {
         Map<String, Object> map = new HashMap<>();
-        map.put("name","张三");
-        map.put("age",25);
-        map.put("salary",10000.00);
+        map.put("name", "张三");
+        map.put("age", 25);
+        map.put("salary", 10000.00);
         return map;
     }
 
@@ -122,24 +125,63 @@ public class AlphaController {
         List<Map<String, Object>> list = new ArrayList<>();
 
         Map<String, Object> map = new HashMap<>();
-        map.put("name","张三");
-        map.put("age",25);
-        map.put("salary",10000.00);
+        map.put("name", "张三");
+        map.put("age", 25);
+        map.put("salary", 10000.00);
         list.add(map);
 
         map = new HashMap<>();
-        map.put("name","李四");
-        map.put("age",30);
-        map.put("salary",12000.00);
+        map.put("name", "李四");
+        map.put("age", 30);
+        map.put("salary", 12000.00);
         list.add(map);
 
         map = new HashMap<>();
-        map.put("name","王五");
-        map.put("age",20);
-        map.put("salary",20000.00);
+        map.put("name", "王五");
+        map.put("age", 20);
+        map.put("salary", 20000.00);
         list.add(map);
 
         return list;
     }
 
+    // cookie示例
+    @RequestMapping(path = "/cookie/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        // 创建cookie
+        Cookie cookie = new Cookie("code", CommunityUtil.generateUUID());
+        // 设置cookie生效的范围
+        cookie.setPath("/community/alpha");
+        // 设置cookie存储时间
+        cookie.setMaxAge(60 * 10);
+        // 发送cookie
+        response.addCookie(cookie);
+
+        return "set cookie";
+    }
+
+    @RequestMapping(path = "/cookie/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code") String code) {
+        System.out.println(code);
+        return "get cookie";
+    }
+
+    // session示例
+    @RequestMapping(path = "/session/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session) {
+        session.setAttribute("id", 1);
+        session.setAttribute("name", "Test");
+        return "set session";
+    }
+
+    @RequestMapping(path = "/session/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session) {
+        System.out.println(session.getAttribute("id"));
+        System.out.println(session.getAttribute("name"));
+        return "get session";
+    }
 }
